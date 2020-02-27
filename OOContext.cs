8c3649f4a762e6b7b29a30c79c1fcccd500e171b
Mia <miahljnh@gigaming.io>
2020-02-27 13:34:26 +0700

    .

--- OOContext.cs
+++ OOContext.cs
@@ -4,15 +4,37 @@ using TryoutFebruari.Models;
 
 namespace TryoutFebruari
 {
-    public class OnlineOrderContext : DbContext
+    public class CustomerContext : DbContext
     {
-        public OnlineOrderContext(DbContextOptions<OnlineOrderContext> options) : base(options) { }
+        public CustomerContext(DbContextOptions<CustomerContext> options) : base(options) { }
 
         public DbSet<Customers> customers { get; set; }
-        //public DbSet<Orders> orders { get; set; }
-        //public DbSet<Order_items> order_Items { get; set; }
-        //public DbSet<Drivers> drivers { get; set; }
-        //public DbSet<Products> products { get; set; }
+    }
 
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options) { }
+
+        public DbSet<Orders> orders { get; set; }
+    }
+
+    public class OrderItemContext : DbContext
+    {
+        public OrderItemContext(DbContextOptions<OrderItemContext> options) : base(options) { }
+
+        public DbSet<Order_items> order_Items { get; set; }
+    }
+
+    public class DriversContext : DbContext
+    {
+        public DriversContext(DbContextOptions<DriversContext> options) : base(options) { }
+
+        public DbSet<Drivers> drivers { get; set; }
+    }
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+
+        public DbSet<Products> products { get; set; }
     }
 }
